17ca51f6285f85bf090af25240c6b10ccf27e4ee
20120454 <dat13102k2@gmail.com>
2024-06-09 00:17:02 +0700

    update

--- test/index.test.js
+++ test/index.test.js
@@ -1,16 +1,24 @@
-var request = require('supertest');
-var express = require('express');
- 
-var app = require('../app');
- 
-describe('Index Page', function() {
-  it("renders successfully", function(done) {
-    request(app).get('/').expect(200, done);    
-  })
-})
+const request = require('supertest');
+const app = require('../app');
 
-describe('Trains API', function() {
-  it("returns data successfully", function(done) {
-    request(app).get('/trains').expect(200).expect('Content-Length', '1093').expect('Content-Type', /json/, done); ;    
-  })
-})
+describe('Index Page', () => {
+  it('renders successfully', (done) => {
+    request(app)
+      .get('/')
+      .expect(200, done);    
+  });
+});
+
+describe('Trains API', () => {
+  it('returns data successfully', (done) => {
+    request(app)
+      .get('/trains')
+      .expect(200)
+      .expect('Content-Length', '1093')
+      .expect('Content-Type', /json/)
+      .end((err, res) => {
+        if (err) return done(err);
+        done();
+      });
+  });
+});
